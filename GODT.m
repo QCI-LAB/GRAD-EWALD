bec2d1e9267d8aefd7418cf35ae47d01941cfeba
JuwWin <julkosta@gmail.com>
2025-05-07 09:35:28 +0200

    Provide the processing path for gradient data

---
+++ GODT.m
@@ -0,0 +1,126 @@
+%% Tomographic Reconstruction using gradient projections
+% Based on the work of: Piotr Makowski, Wojciech Krauze, Paweł Ossowski
+% Author: Julianna Winnik
+% Warsaw University of Technology, 2025
+
+clear; clc;
+close all
+[path, ~, ~] = fileparts(mfilename('fullpath'));
+addpath(genpath(path));
+
+%%%%%%%%% START OF PARAMETERS SECTION:
+
+%% Reconstruction Presets
+DI; ... % direct inversion (no iterations)
+
+%% Optimization parameters
+% After filling all projections: there is no useful information in 'z' far from center, so we can crop it by downsampling Kspace before IFT - must be power of 2
+ROI_crop_z = 2; % 1 / 2 / 4 (1 - no crop)
+% Resolution in 'z' is 4x worse then 'xy' so before IFT, Kspace can be cropped in 'z' to 0.25 of its original size
+limit_resolution_z = 0.5; % 0.25 / 0.5 / 1 (1 - no crop)
+sinogram_subset_factor = 1;     % default = 1     % take every Nth projection from sinogram
+projection_crop_factor = 1;     % default = 1;    % crop factor (<=1)
+resample_projections   = false; % default = false % resample projections to minimal safe resolution
+
+%% Save reconstruction?
+save_reconstruction    = 1;     % 0 - don't save; 1 - save RI; 2 - save RI and absorption (complex RI)
+% TODO: it should be 1 in final version
+
+%% Plots
+% show selected plots:
+% '0' : off
+% '1' : show final reconstruction
+% '2' : show spectrum of each processed projection
+% '3' : show fully filled K-space, Direct Inversion result and object support (if applies)
+% '4' : show reconstruction (XZ) progress in GP and 1D plot of relative progress of GP
+% '5' : show selected plots of reconstruction output and save them to .png (Plots.m script)
+% 'D' : debug mode - detailed plots not shown in other modes:
+%                  - show the process of filling K-space with projections
+% 'DD': extended debug mode:
+%                  - show the process of filling K-space with projections
+%                  - show the process of filling K-space with projections- also show spectrum of projection with dimmed out frequencies aroud NA
+plots = '1';
+%JW TODO; it shold be 1 in final version
+%%%%%%%%% END OF PARAMETER SECTION
+
+%% Load sinogram and system basic parameters
+loadfiles;
+
+%% Assume defaults for unspecified things
+defaults;
+
+%% Ensure compatibility with Gradient Optical Diffraction Tomography (GODT)
+
+if ~exist('SINOamp', 'var')
+    SINOamp = zeros(size(SINOph)); 
+    warning('Shearing amplitude not measured; assuming uniform amplitude, which results in a zero derivative.');
+else
+    warning('It is assumed that SINOamp contains (amp - shifted_amp) / amp.');
+end
+
+% Check for shear variable
+if ~exist('shear', 'var')
+    shear = 1;
+    warning('Variable "shear" not found. Setting shear = 1. The result will not be quantitative.');
+end
+
+% Enforce supported approximation mode
+if ~exist('Approx', 'var') || ~strcmp(Approx, 'GradRytov')
+    Approx = 'GradRytov';
+    warning('Only "GradRytov" mode is supported when input data comes from shearing interferometry. Forcing Approx = "GradRytov".');
+end
+
+
+%% Preparing data for reconstruction
+[SINOamp_reduced,SINOph_reduced, sino_params, dx, ...
+ projection_padding_xy, N_projection_padded, ...
+ x_tv, n_obj_res, plots, Fpmask, projection_downsample_factor, N_SINO_y] = ...
+	Preprocess(SINOamp, SINOph, geometry, sino_params, resample_projections, dx, projection_crop_factor, Fpmask,...
+                n_obj, thetay, sinogram_subset_factor, n_imm, Masking, N_CP, nCPi,...
+                projection_padding_xy, plots, do_NNC);
+
+%% Calculating reconstruction   
+[RECON, dx, dz, nGPi, N_Kspace_xy_padded, KO,KOi, RMAEtab,RRMSEtab,RMADtab,RRMSDtab] = ...
+	FDT(SINOamp_reduced,SINOph_reduced, sino_params, thetay, ... % sinogram (including index of OCT proj)
+		n_imm,dx, ... % optical system (ODT and OCT)
+        shear, ... % shear value
+		geometry,Approx,interpFp,Ramp,do_NNC, ... % solver approximations
+		projection_padding_xy, Kspace_padding, N_projection_padded, Kspace_oversampling_z, ROI_crop_z, limit_resolution_z, ... % Fourier space sampling
+		nGPi,epsi,relaxGP,relaxM, ... % Gerchberg-Papoulis iterationsnGPi
+		x_tv, n_obj_res, ... % Gerchberg-Papoulis object constraints/reference
+		plots, Fpmask);
+
+%% Display and save
+time=toc;
+sim_time = show_time(time);
+
+if save_reconstruction
+    save_recon;
+end
+
+if contains(plots,'1'); vis(real(RECON), [min(real(RECON(:))), max(real(RECON(:)))]); colormap jet; end
+if contains(plots,'5'); Plots; end
+
+%% display
+
+rec_size=size(RECON);
+x=(-rec_size(1)/2+1:rec_size(1)/2)*dx;
+dz = dx/limit_resolution_z;
+z=(-rec_size(3)/2+1:rec_size(3)/2)*dz;
+
+vidfile = VideoWriter(['video.mp4'],'MPEG-4');
+vidfile.FrameRate = 3;
+open(vidfile);
+f=figure;
+% f.Units = 'normalized';
+% f.OuterPosition = [0,0.05,1,0.95];
+ri_range=[min(real(RECON(:))),max(real(RECON(:)))]/4;
+m=50;
+for i_z = rec_size(3)/2+1-m:rec_size(3)/2+m
+    imagesc(x,x,squeeze(real(RECON(:,:,i_z))),ri_range);colorbar
+    title(sprintf('real z = %2.2f um',z(i_z)));colorbar;axis image;
+    xlabel('x [um]');ylabel('y [um]');
+    F(i_z) = getframe(gcf); 
+    writeVideo(vidfile,F(i_z));
+end
+close(vidfile)
